6b2b3752a9b34998a883d25815de04a1c4c5809e
The Zuraiz <thezuraiz@gmail.com>
2024-10-25 18:34:56 +0500

    Add Visitor in GYM Model

--- lib/features/authentication/models/gym_model.dart
+++ lib/features/authentication/models/gym_model.dart
@@ -13,6 +13,7 @@ class Gym {
   final OwnerBankDetails ownerBankDetails;
   final double balance;
   final bool isApproved;
+  final List<Visitor> visitors; // New field to track gym visitors
 
   Gym({
     required this.id,
@@ -29,6 +30,7 @@ class Gym {
     required this.ownerBankDetails,
     this.balance = 0.0,
     this.isApproved = false,
+    this.visitors = const [], // Default to an empty list
   });
 
   // Convert Gym object to JSON
@@ -47,6 +49,8 @@ class Gym {
         "owner_bank_details": ownerBankDetails.toJson(),
         "balance": balance,
         "isApproved": isApproved,
+        "visitors": visitors.map((v) => v.toJson()).toList(),
+        // Convert each visitor to JSON
       };
 
   // Create Gym object from JSON
@@ -65,6 +69,40 @@ class Gym {
         ownerBankDetails: OwnerBankDetails.fromJson(json["owner_bank_details"]),
         balance: (json["balance"] ?? 0.0).toDouble(),
         isApproved: json["isApproved"] ?? false,
+        visitors: (json["visitors"] as List<dynamic>?)
+                ?.map((visitor) => Visitor.fromJson(visitor))
+                .toList() ??
+            [], // Parse each visitor JSON
+      );
+}
+
+// Define a Visitor model to track each visitor's details
+class Visitor {
+  final String userId;
+  final DateTime checkInTime;
+  final DateTime?
+      checkOutTime; // Optional, can be null if user hasn't checked out
+
+  Visitor({
+    required this.userId,
+    required this.checkInTime,
+    this.checkOutTime,
+  });
+
+  // Convert Visitor object to JSON
+  Map<String, dynamic> toJson() => {
+        "user_id": userId,
+        "check_in_time": checkInTime.toIso8601String(),
+        "check_out_time": checkOutTime?.toIso8601String(),
+      };
+
+  // Create Visitor object from JSON
+  factory Visitor.fromJson(Map<String, dynamic> json) => Visitor(
+        userId: json["user_id"],
+        checkInTime: DateTime.parse(json["check_in_time"]),
+        checkOutTime: json["check_out_time"] != null
+            ? DateTime.parse(json["check_out_time"])
+            : null,
       );
 }
 
